1cc8899a56fac87900800360008c8a79f7fe99c0
Manuel Alvarado <saimon.03.architect@gmail.com>
2024-05-28 11:51:01 -0400

    angular-Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    // provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,6 @@
+
+
+
+export const environments = {
+  baseUrl: 'https://Saimon-Alvarado.com/api'
+}
